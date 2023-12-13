17c9165ed6be23f43e0a2e16a310e6b6705dc6ac
instant.sc <code@insc.cc>
2023-12-13 12:10:22 +0100

    Add project files.

---
+++ UltimatumCheck.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using ExileCore;
+using ExileCore.PoEMemory;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.Elements;
+using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared.Cache;
+using ExileCore.Shared.Enums;
+using ExileCore.Shared.Helpers;
+using SharpDX;
+
+namespace UltimatumCheck;
+
+public class UltimatumCheck : BaseSettingsPlugin<UltimatumCheckSettings>
+{
+    private readonly ConditionalWeakTable<Entity, CachedValue<Element>> _entityMapping = new();
+
+    public override bool Initialise()
+    {
+        return true;
+    }
+
+    public override Job Tick()
+    {
+        return null;
+    }
+
+    public override void Render()
+    {
+        if (GameController.EntityListWrapper.ValidEntitiesByType[EntityType.IngameIcon]
+                    .FirstOrDefault(x => x.Path == "Metadata/Terrain/Leagues/Ultimatum/Objects/UltimatumChallengeInteractable") is
+                { } encounterEntity && encounterEntity.TryGetComponent<StateMachine>(out var stateMachine) &&
+            stateMachine.States.FirstOrDefault(x => x.Name == "encounter_started")?.Value is 0 or null)
+        {
+            var element = _entityMapping.GetValue(encounterEntity,
+                    ee => new TimeCache<Element>(() => GameController.IngameState.IngameUi.ItemsOnGroundLabelsVisible.FirstOrDefault(x => Equals(x.ItemOnGround, ee))?.Label,
+                        1000))!
+                .Value;
+            var choicesPanel = element?.GetChildFromIndices(0, 0, 2)?.AsObject<UltimatumChoicePanel>();
+            if (choicesPanel is { IsVisible: true })
+            {
+                DrawModifierOptions(choicesPanel);
+            }
+        }
+
+        if (GameController.IngameState.IngameUi.UltimatumPanel is { IsVisible: true, ChoicesPanel: { } panel })
+        {
+            DrawModifierOptions(panel);
+        }
+    }
+
+    private void DrawModifierOptions(UltimatumChoicePanel panel)
+    {
+        foreach (var ((element, modifier), index) in panel.ChoiceElements.Zip(panel.Modifiers).Select((x, i) => (x, i)))
+        {
+            Color modColor = Settings.UltimatumModRanking.GetModifierTier(modifier.Id) switch
+            {
+                1 => Settings.Rank1Color,
+                2 => Settings.Rank2Color,
+                3 => Settings.Rank3Color,
+                _ => Color.Gray
+            };
+            Graphics.DrawFrame(
+                element.GetClientRectCache.TopLeft.ToVector2Num(),
+                element.GetClientRectCache.BottomRight.ToVector2Num(),
+                modColor, Settings.FrameThickness + (panel.SelectedChoice == index ? 5 : 0));
+        }
+    }
+}
---
+++ UltimatumCheckSettings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExileCore.PoEMemory;
+using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using ImGuiNET;
+using SharpDX;
+
+namespace UltimatumCheck;
+
+public class UltimatumCheckSettings : ISettings
+{
+    //Mandatory setting to allow enabling/disabling your plugin
+    public ToggleNode Enable { get; set; } = new(false);
+
+    //Put all your settings here if you can.
+    //There's a bunch of ready-made setting nodes,
+    //nested menu support and even custom callbacks are supported.
+    //If you want to override DrawSettings instead, you better have a very good reason.
+    public ColorNode Rank1Color { get; set; } = new(Color.Green);
+    public ColorNode Rank2Color { get; set; } = new(Color.Yellow);
+    public ColorNode Rank3Color { get; set; } = new(Color.Red);
+    public RangeNode<int> FrameThickness { get; set; } = new(3, 0, 10);
+    public UltimatumModRanking UltimatumModRanking { get; set; } = new();
+}
+
+[Submenu(RenderMethod = nameof(Render))]
+public class UltimatumModRanking
+{
+    public Dictionary<string, int> TierMapping = [];
+    private string _roomFilter = "";
+
+    public void Render()
+    {
+        ImGui.InputTextWithHint("##ModFilter", "Filter", ref _roomFilter, 100);
+        foreach (var mod in RemoteMemoryObject.pTheGame.Files.UltimatumModifiers.EntriesList
+                     .Where(t => t.Name.Contains(_roomFilter, StringComparison.OrdinalIgnoreCase) ||
+                                 t.Description.Contains(_roomFilter, StringComparison.OrdinalIgnoreCase)))
+        {
+            var currentValue = GetModifierTier(mod.Id);
+            if (ImGui.SliderInt($"{(string.IsNullOrWhiteSpace(mod.Name) ? mod.Id : mod.Name)}###{mod.Id}", ref currentValue, 1, 3))
+            {
+                TierMapping[mod.Id] = currentValue;
+            }
+
+            if (!string.IsNullOrWhiteSpace(mod.Description) && ImGui.BeginItemTooltip())
+            {
+                ImGui.TextUnformatted(mod.Description);
+                ImGui.EndTooltip();
+            }
+        }
+    }
+
+    public int GetModifierTier(string id)
+    {
+        return TierMapping.GetValueOrDefault(id, 2);
+    }
+}
